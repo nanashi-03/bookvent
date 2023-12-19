6eddc44ed36cbb623aa1991777dfb7259beda388
Nanashi <40239873+nanashi-03@users.noreply.github.com>
2023-12-19 19:14:15 +0530

    Continued

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -8,10 +8,41 @@ import { cookies } from 'next/headers'
 //     url: UrlObject;
 // };
 
+const books = [
+    {
+        title: "Tress of the Emerald Sea",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/Tress%20of%20the%20Emerald%20Sea.pdf?t=2023-12-19T12%3A45%3A46.119Z"
+    },
+    {
+        title: "Pachinko",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/Pachinko%20A%20Novel.pdf"
+    },
+    {
+        title: "The Sword of Kaigen",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/The%20Sword%20of%20Kaigen%20(M.L.%20Wang)%20(Z-Library).pdf?t=2023-12-14T10%3A16%3A46.524Z"
+    },
+    {
+        title: "Fourth Wing (The Empyrean Book 01)",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/Fourth%20Wing%20(The%20Empyrean%20Book%2001).pdf?t=2023-12-19T12%3A43%3A43.532Z"
+    },
+    {
+        title: "The Travelling Cat Chronicles",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/The%20Travelling%20Cat%20Chronicles.pdf"
+    },
+    {
+        title: "The Anthropocene Reviewed Essays on a Human-Centered Planet",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/The%20Anthropocene%20Reviewed%20Essays%20on%20a%20Human-Centered%20Planet.pdf?t=2023-12-19T12%3A44%3A40.269Z"
+    },
+    {
+        title: "Norse Mythology",
+        url: "https://ytymycdsazkxmwuuvfsz.supabase.co/storage/v1/object/public/Books/Norse%20Mythology.pdf" 
+    }
+]
+
 export default async function Page() {
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
-    const { data: books }:{ data: any } = await supabase.from('Books').select("*");
+    //const { data: books }:{ data: any } = await supabase.from('Books').select("*");
     const {
         data: { user },
     } = await supabase.auth.getUser()
